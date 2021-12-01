5cbedac3e68f5b2312ff320930315d557fb3cf4a
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2021-12-01 10:48:52 +0100

    divers

---
+++ eclipse-workspace/Exomoyenne/src/moyenne.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class moyenne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//traduction du pseudo code en code Java
+		
+		//DECLARATION DES VARIABLES
+		
+		//Réel nombre1
+		float nombre1;
+		//Réel nombre2
+		float nombre2;
+		//Réel moyenne
+		float moyenne;
+		
+		//SAISIES
+		//ecrire "Donnez moi svp le premier nombre:"
+		System.out.println("Donnez moi svp le premier nombre:");
+		//Lire nombre1
+		Scanner sc = new Scanner(System.in);//creer un outil scanner(UNE SEULE FOIS SUFFIT)
+		nombre1 = sc.nextFloat();
+		System.out.println("Vous avez saisie le nombre1="+nombre1);
+		
+		//ecrire "Donnez moi svp le nombre2:"
+		System.out.println("Donnez moi svp le nombre2:");
+		//Lire nombre2
+		nombre2 = sc.nextFloat();
+		System.out.println("Vous avez saisie le nombre2="+nombre2);
+		
+		//TRAITEMENT
+		
+		moyenne = (nombre1+nombre2)/2;
+		
+		
+		//AFFICHAGE
+		//ecrire "la valeur moyenne est :",moyenne
+		
+		
+	}
+
+}
