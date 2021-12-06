8e38f4e76d3b629501c815ca0799338209f4c65f
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2021-12-06 09:41:42 +0100

    update exo java airerayonsphere

--- eclipse-workspace/airerayonplusangle/src/airerayonplusangle/calculairerayonplusangle.java
+++ eclipse-workspace/airerayonplusangle/src/airerayonplusangle/calculairerayonplusangle.java
@@ -1,10 +1,30 @@
 package airerayonplusangle;
 
+import java.util.Scanner;
+
 public class calculairerayonplusangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		//DECLARATION DES VARIABLES
+		//Double rayon
+		double rayon;
+		//Double anglea
+		double anglea;
+		//Double aire
+		double aire;
+		
+		//SAISIES
+		//ecrire "donnez moi le rayon svp:"
+		System.out.println("Donnez moi le rayon svp:");
+		//lire "rayon"
+		Scanner sc = new scanner(System.in);
+		r = sc.nextDouble();
 
+		sc.close();
+		
 	}
 
 }
