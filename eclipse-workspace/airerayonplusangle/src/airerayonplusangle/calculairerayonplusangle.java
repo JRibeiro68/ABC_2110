63f2394f92e711b98c738b545bbf23c4b5b75697
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2021-12-06 16:40:06 +0100

    update exo java

--- eclipse-workspace/airerayonplusangle/src/airerayonplusangle/calculairerayonplusangle.java
+++ eclipse-workspace/airerayonplusangle/src/airerayonplusangle/calculairerayonplusangle.java
@@ -10,31 +10,35 @@ public class calculairerayonplusangle {
 		
 		//DECLARATION DES VARIABLES
 		//Double r
-		double rayon;
-		//Double anglea
-		double anglea;
+		double r;
+		//Double a
+		double a;
 		//Double aire
 		double aire;
+	
 		
 		
 		//SAISIES
 		//ecrire "donnez moi le rayon svp:"
 		System.out.println("Donnez moi le rayon svp:");
 		//lire "r"
-		Scanner sc = new scanner(System.in);
-		rayon = sc.nextDouble();
+		Scanner sc = new Scanner(System.in);
+		r = sc.nextDouble();
 		//ecrire "donnez moi l'angle a en degrés svp:"
 		System.out.println("donnez moi l'angle a en degrés svp");
-		//lire "anglea"
+		//lire "a"
+		a = sc.nextDouble();
 		
 		
 		//TRAITEMENTS
 		//aire <-- Math.PI*Math.pow(r,2)*a/360
-		aire = (Math.PI*Math.pow(r,2)*a/360);
+		aire = (a/360*Math.PI*Math.pow(r,2));
 		
 		//AFFICHAGE
-		//ecrire "l'aire du secteur circulaire est de :",aire
-		System.out.println(l'aire du secteur circulaire est de "+aire);
+		//ecrire "l'aire du secteur circulaire est de :"+aire
+		System.out.println("l'aire du secteur circulaire est de :"+aire);
+		
+				
 		
 
 		sc.close();
