b890841ee83449d8d5a5c11cf57e7f70a6ea173d
JRibeiro68 <92739695+JRibeiro68@users.noreply.github.com>
2021-12-07 19:26:51 +0100

    exo java inverser a et b

---
+++ eclipse-workspace/inverseraetb/src/inverseraetb/Inversernombre.java
@@ -0,0 +1,53 @@
+package inverseraetb;
+
+import java.util.Scanner;
+
+public class Inversernombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//DECLARATION DES VARIABLES
+		//int a
+		int a;
+		//int b
+		int b;
+		//int c
+		int c;
+		
+		
+		//SAISIES
+		//ecrire "combien vaut a?"
+		System.out.println("combien vaut a?");
+		//lire "a"
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		//ecrire "combien vaut b?"
+		System.out.println("combien vaut b?");
+		//lire "b"
+		b = sc.nextInt();
+
+		
+		//TRAITEMENTS
+		//c <-- b
+		c = b;
+		//b <-- a
+		b = a;		
+		//a <-- b
+		a = b;
+		
+		
+		//AFFICHAGE
+		//ecrire "a= "+b
+		System.out.println("a= "+c);
+		//ecrire "b= "+a
+		System.out.println("b= "+a);
+		
+		
+	
+		sc.close();
+		
+	}
+
+}
