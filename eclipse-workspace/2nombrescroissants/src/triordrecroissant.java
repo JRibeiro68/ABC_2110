63a4be194a4aa2d8b63f07ebf5e502475dff400c
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2022-01-10 14:08:04 +0100

    update exo java

---
+++ eclipse-workspace/2nombrescroissants/src/triordrecroissant.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class triordrecroissant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//DECLARATION DES VARIABLES
+		
+		//Entier	nombrea
+		int			nombrea;
+		//Entier	nombreb
+		int			nombreb;
+		
+		
+		//SAISIES
+		//ecrire "Entrez le nombre a:"
+		System.out.println("Entrez le nombre a:");
+		//lire "nombrea"
+		Scanner sc = new Scanner(System.in);
+		nombrea = sc.nextInt();
+		
+		//ecrire "Entrez le nombre b:"
+		System.out.println("Entrez le nombre b");
+		//lire "nombreb"
+		nombreb = sc.nextInt();
+		
+		
+		//TRAITEMENT
+		//Si	(nombrea < nombreb)
+		if		(nombrea<nombreb) {
+			//ecrire "nombrea < nombreb"
+			System.out.println(nombrea +"<"+ nombreb);
+		}
+		else if
+		
+		sc.close();
+	}
+
+}
