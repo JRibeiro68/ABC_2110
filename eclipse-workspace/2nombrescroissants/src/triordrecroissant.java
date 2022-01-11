5150cb7f9b02bd6f9a8081b2054d5074066807e6
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2022-01-11 12:50:53 +0100

    Update triordrecroissant.java

--- eclipse-workspace/2nombrescroissants/src/triordrecroissant.java
+++ eclipse-workspace/2nombrescroissants/src/triordrecroissant.java
@@ -4,36 +4,46 @@ public class triordrecroissant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
+
 		//DECLARATION DES VARIABLES
-		
+
 		//Entier	nombrea
 		int			nombrea;
 		//Entier	nombreb
 		int			nombreb;
-		
-		
+
+
 		//SAISIES
 		//ecrire "Entrez le nombre a:"
 		System.out.println("Entrez le nombre a:");
 		//lire "nombrea"
 		Scanner sc = new Scanner(System.in);
 		nombrea = sc.nextInt();
-		
+
 		//ecrire "Entrez le nombre b:"
 		System.out.println("Entrez le nombre b");
 		//lire "nombreb"
 		nombreb = sc.nextInt();
-		
-		
+
+
 		//TRAITEMENT
 		//Si	(nombrea < nombreb)
 		if		(nombrea<nombreb) {
 			//ecrire "nombrea < nombreb"
 			System.out.println(nombrea +"<"+ nombreb);
 		}
-		else if
+		//Sinon Si(nombrea > nombreb)
+		else if	(nombrea>nombreb) {
+			//ecrire "nombreb < nombrea"
+			System.out.println(nombreb +"<"+ nombrea);
+		}
+		//Sinon Si(nombrea = nombreb)
+		else if (nombrea==nombreb) {
+			//ecrire "nombrea = nombreb"
+			System.out.println(nombrea +" et "+ nombreb +" sont égaux.");
+		}
+		
 		
 		sc.close();
 	}
