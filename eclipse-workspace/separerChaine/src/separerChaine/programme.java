ee0091b8fb22327bc38466a17c98a632d2bd409e
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2022-02-07 11:44:34 +0100

    update

--- eclipse-workspace/separerChaine/src/separerChaine/programme.java
+++ eclipse-workspace/separerChaine/src/separerChaine/programme.java
@@ -1,9 +1,23 @@
 package separerChaine;
 
+import java.util.Scanner;
+
 public class programme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String saisie;
+		Scanner sc = new Scanner(system.in);
+		
+		System.out.println("Entrez une valeur saisie de MI ou KM:");
+		
+		saisie = sc.nextLine();
+		
+		
+		
+		sc.close();
+
 
 	}
 
