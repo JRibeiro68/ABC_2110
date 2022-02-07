2487cb0dcec96aa01894889f0060a209de888c3d
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2022-02-07 13:50:30 +0100

    update

--- eclipse-workspace/separerChaine/src/separerChaine/programme.java
+++ eclipse-workspace/separerChaine/src/separerChaine/programme.java
@@ -8,14 +8,38 @@ public class programme {
 		// TODO Auto-generated method stub
 		
 		String saisie;
+		String[] tableauDeConversion;
+		int valeur;
+		String uniteDeMesure;
+		double conversion;
+	
 		Scanner sc = new Scanner(system.in);
 		
 		System.out.println("Entrez une valeur saisie de MI ou KM:");
 		
-		saisie = sc.nextLine();
+		saisie = sc.nextLine(); // "12 mi"
+		tableauDeConversion = saisie.split(" ");
+		valeur = Integer.valueOf(tableauDeConversion[0]);
+		if(tableauDeConversion.length > 1) {
+			uniteDeMesure = tableauDeConversion[1];
+		}
+		else {
+				uniteDeMesure = "km";
+		}
+		uniteDeMesure = uniteDeMesure.toLowerCase();
+		if(uniteDeMesure.equals("km")) {
+			//conversion en km vers mi
+			conversion= (valeur/1.609);
+			System.out.println("La distance "+valeur+" en km vaut "+conversion+" miles");
+		}
+		else {
+				//conversion mi vers km
+				conversion= (valeur*1.609);
+				System.out.println("La distance "+valeur+" en miles vaut "+conversion+" km");
+		}
 		
 		
-		
+		//affichage du résultat
 		sc.close();
 
 
