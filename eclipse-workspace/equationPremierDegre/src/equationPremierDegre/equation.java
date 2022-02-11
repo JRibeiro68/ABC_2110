0235f0b5d777d260bb3a9d09380f7dabe454565a
RastaHyoga <92739695+RastaHyoga@users.noreply.github.com>
2022-02-11 11:52:13 +0100

    update java

---
+++ eclipse-workspace/equationPremierDegre/src/equationPremierDegre/equation.java
@@ -0,0 +1,40 @@
+package equationPremierDegre;
+
+import java.util.Scanner;
+
+public class equation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a,b,x;
+		int 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Entrez a :");
+		a = sc.nextDouble();
+
+		System.out.println("Entrez b :");
+		b = sc.nextDouble();
+
+		System.out.println("Entrez x :");
+		x = sc.nextDouble();
+
+		if ((a !=0) && (b !=0)) {
+			0 = (a*x+b);
+			else if ((a =0) && (b =0)) {
+				System.out.println("L'équation est indéterminée");
+			}
+
+
+
+		}
+			
+		
+		
+		sc.close();
+
+	}
+
+}
